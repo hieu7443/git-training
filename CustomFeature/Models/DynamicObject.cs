8a92f85bf0dcd012e61fd9ccda835b8a6eff8c8d
hieu.nguyentrung <hieu.nguyentrung@sutrixsolutions.com>
2018-07-12 18:06:07 +0700

    update

---
+++ CustomFeature/Models/DynamicObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CustomFeature.Models
+{
+    public interface IDynamicObject
+    {
+        bool IsObject(string key);
+        DynamicObject GetObject(string key);
+        T GetObject<T>(string key) where T : DynamicObject;
+        object GetValue(string key);
+        T GetValue<T>(string key);
+    }
+    public class DynamicObject : Dictionary<string, object>, IDynamicObject
+    {
+        public bool IsObject(string key)
+        {
+            return this[key] is IDynamicObject;
+        }
+        public DynamicObject GetObject(string key)
+        {
+            return (DynamicObject)this[key];
+        }
+        public T GetObject<T>(string key) where T : DynamicObject
+        {
+            return (T)this[key];
+        }
+        public object GetValue(string key)
+        {
+            return this[key];
+        }
+        public T GetValue<T>(string key)
+        {
+            return (T)this[key];
+        }
+    }
+}
--- CustomFeature/TwitterApi/TwitterData.cs
+++ CustomFeature/TwitterApi/TwitterData.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using CustomFeature.Models;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,64 +7,11 @@ using System.Web;
 
 namespace CustomFeature.TwitterApi
 {
-    public class TwitterData : Dictionary<string, object>
+    public class TwitterData : DynamicObject
     {
-        public TwitterData(JToken data)
-        {
-            foreach (var child in data)
-            {
-                var property = (JProperty)child;
-                this.Add(property.Name, GetValue(property.Value));
-            }
-        }
-        private object GetValue(JToken data)
-        {
-            object value = null;
-            switch (data.Type)
-            {
-                case JTokenType.Array:
-                    value = data.Select(i => GetValue(i)).ToArray();
-                    break;
-                case JTokenType.Object:
-                    value = GetProperTy(data);
-                    break;
-                case JTokenType.Boolean:
-                    value = data.Value<bool>();
-                    break;
-                case JTokenType.Bytes:
-                    value = data.Value<byte[]>();
-                    break;
-                case JTokenType.Date:
-                    value = data.Value<DateTime>();
-                    break;
-                case JTokenType.Float:
-                    value = data.Value<double>();
-                    break;
-                case JTokenType.Guid:
-                    value = data.Value<Guid>();
-                    break;
-                case JTokenType.Integer:
-                    value = data.Value<long>();
-                    break;
-                case JTokenType.TimeSpan:
-                    value = data.Value<TimeSpan>();
-                    break;
-                case JTokenType.Uri:
-                    value = data.Value<Uri>();
-                    break;
-                default:
-                    value = data.Value<string>();
-                    break;
-            }
-            return value;
-        }
-        private Dictionary<string, object> GetProperTy(JToken data)
-        {
-            return data.Select(i => (JProperty)i).ToDictionary(i => i.Name, i => GetValue(i.Value));
-        }
         public static TwitterData[] RenderFromTimeline(JToken data)
         {
-            return data.Select(i => new TwitterData(i)).ToArray();
+            return data.Select(i => i.RenderObject<TwitterData>()).ToArray();
         }
     }
 }
